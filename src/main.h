23a5ecd37921de74ab5b760f896747fe8c929d09
andmarti1424 <andmarti@gmail.com>
2015-08-05 10:16:53 -0300

    Added read_argv, scerror, scinfo and scdebug prototypes

--- src/main.h
+++ src/main.h
@@ -2,7 +2,8 @@ int main (int argc, char ** argv);
 int exit_app(int status);
 void create_structures();
 void delete_structures();
-void load_sc(int argc, char ** argv);
+void load_sc();
+void read_argv(int argc, char ** argv);
 void setorder(int i);
 void nopipe();
 void signals();
@@ -13,3 +14,8 @@ void sig_int();
 // SIGWINCH signal - resize of terminal
 void winchg();
 
+void scerror(char * s, ...);
+void scinfo(char * s, ...);
+void scdebug(char * s, ...);
+
+extern FILE * fdoutput; // output file descriptor (stdout or file)
