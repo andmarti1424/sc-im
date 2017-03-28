0ca00126a590345fb8a8f17fc61e6e182e5b4cd3
andmarti1424 <andmarti@gmail.com>
2017-03-27 22:44:47 -0300

    fix in shift

--- src/shift.c
+++ src/shift.c
@@ -131,10 +131,10 @@ void shift_cells_right(int deltarows, int deltacols) {
     if ((maxcol >= maxcols) && !growtbl(GROWCOL, 0, maxcol))
         return;
 
-    int lim = maxcol - curcol + deltacols - 1;
+    int lim = maxcol - curcol - deltacols;
     for (r=currow; r < currow + deltarows; r++) {
         pp = ATBL(tbl, r, maxcol);
-        for (c = lim; --c > deltacols; pp--)
+        for (c = lim; c-- >= 0; pp--)
             if ((pp[0] = pp[-deltacols])) pp[0]->col += deltacols;
 
         pp = ATBL(tbl, r, curcol);
