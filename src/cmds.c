d7146554cc7b7940c84ff977f7a7bb4ee8361fb7
mongo <andmarti@gmail.com>
2017-04-18 16:16:24 -0300

    change in scroll up

--- src/cmds.c
+++ src/cmds.c
@@ -1166,19 +1166,21 @@ void scroll_up(int n) {
     int freezer = freeze_ranges && (freeze_ranges->type == 'r' ||  freeze_ranges->type == 'a') ? 1 : 0;
     int brrow = freezer ? freeze_ranges->br->row : 0;
     int tlrow = freezer ? freeze_ranges->tl->row : 0;
+    int i, r;
 
-    // check what is the last row visible (r)
-    int i = 0, r = offscr_sc_rows-1;
-    while (i < LINES - RESROW - 1 && r < maxrows - 1){
-    //while (i < LINES - RESROW - 1) {
-        r++;
-        if (row_hidden[r]) continue;
-        else if (r < offscr_sc_rows && ! (freezer && r >= tlrow && r <= brrow)) continue;
-        else if (freezer && r > brrow && r <= brrow + center_hidden_rows) continue;
-        else if (freezer && r < tlrow && r >= tlrow - center_hidden_rows) continue;
-        i++;
-    }
     while (n--) {
+        // check what is the last row visible (r)
+        i = 0, r = offscr_sc_rows-1;
+        while (i < LINES - RESROW - 1 && r < maxrows - 1) {
+            //while (i < LINES - RESROW - 1) {
+            r++;
+            if (row_hidden[r]) continue;
+            else if (r < offscr_sc_rows && ! (freezer && r >= tlrow && r <= brrow)) continue;
+            else if (freezer && r > brrow && r <= brrow + center_hidden_rows) continue;
+            else if (freezer && r < tlrow && r >= tlrow - center_hidden_rows) continue;
+            i++;
+        }
+
         if (freezer && center_hidden_rows && r != brrow) {
             center_hidden_rows--;
         } else if (offscr_sc_rows && (!freezer || r > brrow) ) {
@@ -1190,7 +1192,7 @@ void scroll_up(int n) {
             sc_info("cannot scroll no longer");
             break;
         }
-        if (currow == r) {
+        if (currow > r) {
             currow = back_row(1)->row;
             unselect_ranges();
         }
