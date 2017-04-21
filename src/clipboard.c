6d42e8c091377e609c18d0243a4345a1d5e4d544
mongo <andmarti@gmail.com>
2017-04-21 14:48:28 -0300

    added clipboard files

---
+++ src/clipboard.c
@@ -0,0 +1,112 @@
+#include <wchar.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include "sc.h"
+#include "macros.h"
+#include "tui.h"
+#include "clipboard.h"
+#include "cmds.h"
+#include "file.h"
+#include "conf.h"
+
+int copy_to_clipboard(int r0, int c0, int rn, int cn) {
+    if (! strlen(get_conf_value("default_copy_to_clipboard_cmd"))) return -1;
+
+    // create tmp file
+    char template[] = "/tmp/sc-im-clipboardXXXXXX";
+    int fd = mkstemp(template);
+    if (fd == -1) {
+        sc_error("Error while copying to clipboard");
+        return -1;
+    }
+
+    // get temp file pointer based on temp file descriptor
+    FILE * fp = fdopen(fd, "w");
+
+    // save plain text to file
+    save_plain(fp, r0, c0, rn, cn);
+    fclose(fp);
+
+    // copy to clipboard
+    char syscmd[PATHLEN + strlen(get_conf_value("default_copy_to_clipboard_cmd")) + 1];
+    sprintf(syscmd, "%s", get_conf_value("default_copy_to_clipboard_cmd"));
+    sprintf(syscmd + strlen(syscmd), " %s", template);
+    system(syscmd);
+
+    sc_info("Content copied to clipboard");
+
+    // close file descriptor
+    close(fd);
+
+    // remove temp file
+    unlink(template);
+
+    return 0;
+}
+
+// file shall be already open
+int save_plain(FILE * fout, int r0, int c0, int rn, int cn) {
+    int row, col;
+    register struct ent ** pp;
+    wchar_t out[FBUFLEN] = L"";
+    char num [FBUFLEN] = "";
+    char text[FBUFLEN] = "";
+    char formated_s[FBUFLEN] = "";
+    int res = -1;
+    int align = 1;
+
+    for (row = r0; row <= rn; row++) {
+        // ignore hidden rows
+        //if (row_hidden[row]) continue;
+
+        for (pp = ATBL(tbl, row, col = c0); col <= cn; col++, pp++) {
+            // ignore hidden cols
+            //if (col_hidden[col]) continue;
+
+            if (*pp) {
+                num [0] = '\0';
+                text[0] = '\0';
+                out [0] = L'\0';
+                formated_s[0] = '\0';
+                res = -1;
+                align = 1;
+
+                // If a numeric value exists
+                if ( (*pp)->flags & is_valid) {
+                    res = ui_get_formated_value(pp, col, formated_s);
+                    // res = 0, indicates that in num we store a date
+                    // res = 1, indicates a format is applied in num
+                    if (res == 0 || res == 1) {
+                        strcpy(num, formated_s);
+                    } else if (res == -1) {
+                        sprintf(num, "%.*f", precision[col], (*pp)->v);
+                    }
+                }
+
+                // If a string exists
+                if ((*pp)->label) {
+                    strcpy(text, (*pp)->label);
+                    align = 1;                                // right alignment
+                    if ((*pp)->flags & is_label) {            // center alignment
+                        align = 0;
+                    } else if ((*pp)->flags & is_leftflush) { // left alignment
+                        align = -1;
+                    } else if (res == 0) {                    // res must ¿NOT? be zero for label to be printed
+                        text[0] = '\0';
+                    }
+                }
+                pad_and_align(text, num, fwidth[col], align, 0, out);
+
+                fwprintf(fout, L"%ls", out);
+
+            } else {
+                fwprintf(fout, L"%*s", fwidth[col], " ");
+            }
+        }
+        if (row != rn) fwprintf(fout, L"\n");
+    }
+    return 0;
+}
---
+++ src/clipboard.h
@@ -0,0 +1,3 @@
+int copy_to_clipboard(int r0, int c0, int rn, int cn);
+//int save_plain(wchar_t * wout, int r0, int c0, int rn, int cn);
+int save_plain(FILE * fout, int r0, int c0, int rn, int cn);
