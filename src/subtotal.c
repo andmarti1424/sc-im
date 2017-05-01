b2f62651fdfa7626c5340229e1499262b13a618f
andmarti1424 <andmarti@gmail.com>
2017-05-01 11:05:02 -0300

    replace existent subtotals by default

--- src/subtotal.c
+++ src/subtotal.c
@@ -32,7 +32,6 @@
  *  if you want to replace a preexistent subtotals you should use: :rsubtotal A @sum C
  */
 int subtotal(int r, int c, int rf, int cf, int group_col, char * operation, int ope_col, int replace_subtotals) {
-    //sc_debug("%d %d %d %d", r, c, rf, cf);
     // check ope_col and group_col are valid
     if (ope_col < c || ope_col > cf || group_col < c || group_col > cf) return -1;
 
@@ -53,16 +52,32 @@ int subtotal(int r, int c, int rf, int cf, int group_col, char * operation, int 
     swprintf(interp_line + wcslen(interp_line), BUFFERSIZE, L"%s%d \"%ls\"", coltoa(cf), rf, cline);
     send_to_interp(interp_line);
 
-    // TODO traverse the range and replace subtotals (if replace_subtotals is set)
-    // shifting range up
+    // traverse the range and replace subtotals
+    //
+    // TODO replace subtotals only if replace_subtotals is set
+    int i, j, is_subtotal_row;
+    extern int cmd_multiplier;
+    //if (replace_subtotals) {
+    for (i=r+headers_in_first_row; i <= rf; i++) {
+        is_subtotal_row=0;
+        for (j=c; j<cf; j++) {
+            p = *ATBL(tbl, i, j);
+            if (p && p->label && p->label[0] == '+' && p->label[1] == '@') { is_subtotal_row=1; break; }
+        }
+        if (is_subtotal_row) {
+           cmd_multiplier = 1;
+           shift(i, c, i, cf, L'k');
+           i--;
+           rf--;
+        }
+    }
+    //}
 
     // traverse the range and add subtotals
-    int i, new_rows = 0;
-    extern int cmd_multiplier;
+    int new_rows = 0;
     wchar_t cmd[BUFFERSIZE];
     int row_start_range = r + headers_in_first_row;
     for (i=r+headers_in_first_row+1; i <= rf + new_rows + 1; i++) {
-        //sc_debug("r:%d i:%d rf:%d new_rows:%d", r, i, rf, new_rows);
         p = *ATBL(tbl, i-1, group_col);
         q = *ATBL(tbl, i, group_col);
 
@@ -70,17 +85,17 @@ int subtotal(int r, int c, int rf, int cf, int group_col, char * operation, int 
 
         if ( (p && q && p->label && q->label && strcmp(q->label, p->label) != 0)
            || i == rf + new_rows + 1) {
-           //sc_debug("move: %d %d %d %d", i, c, i, cf);
            cmd_multiplier = 1;
            shift(i, c, i, cf, L'j');
 
-           swprintf(cmd, BUFFERSIZE, L"rightstring %s%d = \"%s(%s)\"", coltoa(group_col), i, operation, p->label);
+           swprintf(cmd, BUFFERSIZE, L"rightstring %s%d = \"+%s(%s)\"", coltoa(group_col), i, operation, p->label);
            send_to_interp(cmd);
+
            swprintf(cmd, BUFFERSIZE, L"let %s%d = %s(%s%d:%s%d)", coltoa(ope_col), i, operation,
            coltoa(ope_col), row_start_range, coltoa(ope_col), i-1);
            send_to_interp(cmd);
+           valueize_area(i, ope_col, i, ope_col);
 
-           //ui_update(TRUE);
            new_rows++;
            i++;
            row_start_range = i;
