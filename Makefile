80f6905955c0e2f90197cd550a2f12cdc63b1dd6
Pim Snel <pim@lingewoud.nl>
2020-02-06 02:41:38 +0100

    add first tests

---
+++ Makefile
@@ -0,0 +1,7 @@
+build:
+	make -C src
+
+test:
+	tests/tests.sh
+
+
---
+++ tests/tests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+## SEE https://github.com/lehmannro/assert.sh for usage
+
+set -e
+
+ROOTDIR=`pwd`
+. tests/assert.sh
+
+rm -f str.md
+cd examples/xlsx
+assert_raises "cat xlsx2markdown.sc | ../../src/sc-im --nocurses" 0 ""
+assert "cat str.md|wc -l|awk '{\$1=\$1};1'" "114"
+cd $ROOTDIR
+rm -f str.md
+
+assert_end xlsx
