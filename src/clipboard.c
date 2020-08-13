3da2ea9cf146862effd431622df5216f937683b9
Wim Stockman <wim.stockman@gmail.com>
2020-08-13 07:56:33 +0200

    Solved an issue when copy pasting with ccopy and cpaste When copy pasting with tab delimiter from one sc file to another sc file. I noticed that my numbers where copied as string. So fixed this with first trimming the token and then check for isnumeric If not use the non-trimmed token as string.
    Second issue, I had with copy and pasting between sc files is empty rows are skipped.
    To Prevent this I added the empyfield variable. if it gets true insert an extra tab.

--- src/clipboard.c
+++ src/clipboard.c
@@ -99,10 +99,10 @@ int paste_from_clipboard() {
         while( token != NULL ) {
             if (r > MAXROWS - GROWAMT - 1 || c > ABSMAXCOLS - 1) break;
             clean_carrier(token);
-            //char * st = str_replace (token, " ", ""); //trim
+            char * num = str_replace (token, " ", ""); //trim
             char * st = token;
-            if (strlen(st) && isnumeric(st))
-                swprintf(line_interp, BUFFERSIZE, L"let %s%d=%s", coltoa(c), r, st);
+            if (strlen(num) && isnumeric(num))
+                swprintf(line_interp, BUFFERSIZE, L"let %s%d=%s", coltoa(c), r, num);
             else
                 swprintf(line_interp, BUFFERSIZE, L"label %s%d=\"%s\"", coltoa(c), r, st);
             if (strlen(st)) send_to_interp(line_interp);
@@ -191,6 +191,7 @@ int save_plain(FILE * fout, int r0, int c0, int rn, int cn) {
     char formated_s[FBUFLEN] = "";
     int res = -1;
     int align = 1;
+    int emptyfield=-1;	
 
     for (row = r0; row <= rn; row++) {
         // ignore hidden rows
@@ -199,6 +200,7 @@ int save_plain(FILE * fout, int r0, int c0, int rn, int cn) {
         for (pp = ATBL(tbl, row, col = c0); col <= cn; col++, pp++) {
             // ignore hidden cols
             //if (col_hidden[col]) continue;
+	    emptyfield=-1;
 
             if (*pp) {
                 num [0] = '\0';
@@ -219,6 +221,9 @@ int save_plain(FILE * fout, int r0, int c0, int rn, int cn) {
                         sprintf(num, "%.*f", precision[col], (*pp)->v);
                     }
                 }
+                else {
+                     emptyfield++;
+               }
 
                 // If a string exists
                 if ((*pp)->label) {
@@ -231,6 +236,12 @@ int save_plain(FILE * fout, int r0, int c0, int rn, int cn) {
                     } else if (res == 0) {                    // res must ¿NOT? be zero for label to be printed
                         text[0] = '\0';
                     }
+                }
+                else {
+                     emptyfield++;
+               }
+                if(emptyfield){
+                   fwprintf(fout, L"\t");
                 }
                 if (! atoi(get_conf_value("copy_to_clipboard_delimited_tab"))) {
                     pad_and_align(text, num, fwidth[col], align, 0, out);
