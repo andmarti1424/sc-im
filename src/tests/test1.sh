6959156003d4e9fff1bf0c997b7f1a1bb29214af
Andrés <andmarti@gmail.com>
2021-06-02 13:39:59 -0300

    Added test in /tests

---
+++ src/tests/test1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+## SEE https://github.com/lehmannro/assert.sh for usage
+
+#Exit immediately if a command exits with a non-zero status.
+set -e
+
+NAME=test1
+
+echo -n $NAME": "
+VALGRIND_CMD='valgrind -v --log-file=${NAME}_vallog --tool=memcheck --track-origins=yes --leak-check=full --show-leak-kinds=all --show-reachable=no'
+. assert.sh
+assert "echo GETNUM C2 | $VALGRIND_CMD ../sc-im ${NAME}.sc --nocurses --nodebug --quit_afterload 2>&1 |grep -v '^$\|Interp'" "81"
+
+#TODO: check valgrind log here
+#"in use at exit: 0 bytes in 0 blocks"
+#"All heap blocks were freed -- no leaks are possible"
+
+assert_end test1
