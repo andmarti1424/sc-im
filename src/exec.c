eccd0dcd5621e591ddb2f7998afbc6f196fca290
miguelpacha <38722031+miguelpacha@users.noreply.github.com>
2023-12-15 22:07:56 -0300

    Update exec.c

--- src/exec.c
+++ src/exec.c
@@ -65,68 +65,60 @@
 int exec_cmd (char * line) {
 #ifdef NCURSES
     int waitres;
-
     ui_pause();
-
     int my_pipe[2];
-    if (pipe(my_pipe) == -1) {
-        sc_error("Error creating pipe");
-        getchar();
-        ui_resume();
-        ui_update(TRUE);
-        return -1;
-    }
+    if (pipe(my_pipe) == -1) 
+        return
+            sc_error("Error creating pipe"),
+            getchar(),
+            ui_resume(),
+            ui_update(TRUE),
+            -1;
 
     pid_t child_id = fork();
-    if (child_id == -1) {
-        sc_error("Fork error");
-        getchar();
-        ui_resume();
-        ui_update(TRUE);
-        return -1;
-    }
+    if (child_id == -1)
+        return
+            sc_error("Fork error"),
+            getchar(),
+            ui_resume(),
+            ui_update(TRUE),
+            -1;
 
     if (child_id == 0) {     // we are in the child process
         close(my_pipe[0]);   // child doesn't read
         dup2(my_pipe[1], 1); // redirect stdout
 
         int argc = 1;
-        char * p;
-        for (p = line; *p; p++)
+        for (char * p; = line; *p; p++)
             argc += (*p == ' ');
         char ** argv = calloc(argc+1, sizeof(char*));
-        int i;
-        for (i = 0; i < argc; i++)
+        for (int i = 0; i < argc; i++)
             argv[i] = strsep(&line, " ");
 
         execvp(argv[0], argv);
-
         free(argv);
         sc_error("Error executing command. ");
         exit_app(-1);
-
-    } else {                 // we are in parent process
-        close(my_pipe[1]);   // parent doesn't write
-        char reading_buf[2];
-
-        while (read(my_pipe[0], reading_buf, 1) > 0) {
-            if (!write(1, reading_buf, 1)) {
-                sc_error("Failed to read command output: %s", strerror(errno));
-                exit_app(-1);
-            }
-        }
-
-        close(my_pipe[0]);
-        wait(&waitres);
-        if (system("echo -n 'Press ENTER to return.'") == -1) {
-            sc_error("Failed to show ENTER prompt: %s", strerror(errno));
-            exit_app(-1);
-        }
-
-        getchar();
-        ui_resume();
-        ui_update(TRUE);
+        return 0;
     }
+    // we are in parent process
+    close(my_pipe[1]);   // parent doesn't write
+    char reading_buf[2];
+
+    while (read(my_pipe[0], reading_buf, 1) > 0)
+        if (!write(1, reading_buf, 1))
+            sc_error("Failed to read command output: %s", strerror(errno)),
+            exit_app(-1);
+
+    close(my_pipe[0]);
+    wait(&waitres);
+    if (system("echo -n 'Press ENTER to return.'") == -1)
+        sc_error("Failed to show ENTER prompt: %s", strerror(errno)),
+        exit_app(-1);
+
+    getchar();
+    ui_resume();
+    ui_update(TRUE);
 #endif
     return 0;
 }
